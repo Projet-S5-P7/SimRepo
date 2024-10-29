b528b757d9278e5a58f6b3530b018d0f7d9d9935
Etienne Bellerive-Blais <57814653+EtienneBBlais@users.noreply.github.com>
2024-10-29 14:48:45 -0400

    2nit

--- S5i-Projet/assets/3d/uploads_files_2176038_SHC+Free+Cybertruck/shc_free_cybertruck.gd
+++ S5i-Projet/assets/3d/uploads_files_2176038_SHC+Free+Cybertruck/shc_free_cybertruck.gd
@@ -5,32 +5,32 @@ var max_distance = 3
 var distance_traveled = 0
 var start_position = Vector3()
 
-# Référence au RayCast3D (supprime @export si non nécessaire dans ton projet)
+
 var raycast: RayCast3D = null
 
 func _ready():
 	start_position = position
 	
-	# Assigner le RayCast3D si ce n'est pas fait manuellement
-	raycast = $RayCast3D  # Remplace par le bon chemin si ton RayCast3D n'est pas un enfant direct
+
+	raycast = $RayCast3D 
 	assert(raycast != null, "Le RayCast3D n'a pas été trouvé !")
 	
-	# Activer le raycast pour qu'il commence à détecter
+
 	raycast.enabled = true
 
 func _process(delta):
 	distance_traveled = position.distance_to(start_position)
 
-	# Vérifier si le RayCast3D détecte une collision
+
 	if raycast and raycast.is_colliding():
 		var collision_distance = raycast.get_collision_point().distance_to(position)
 		if collision_distance <= 0.5:
-			speed = 0  # Arrêter l'objet
-			return  # Ne pas déplacer l'objet si l'obstacle est proche
+			speed = 0  
+			return  
 
-	# Si la distance parcourue est inférieure à max_distance, continuer de bouger
+	
 	if distance_traveled < max_distance:
 		var direction = -transform.basis.x.normalized()
 		position += direction * speed * delta
 	else:
-		speed = 0  # Arrêter l'objet une fois qu'il a parcouru la distance maximale
+		speed = 0  
