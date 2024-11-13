ad46600d0a918f292227d9743cf66e6ccb737d6e
Etienne Bellerive-Blais <57814653+EtienneBBlais@users.noreply.github.com>
2024-11-13 12:49:07 -0500

    reset

--- S5i-Projet/temoins.gd
+++ S5i-Projet/temoins.gd
@@ -11,26 +11,16 @@ var detect_color = Color(0, 1, 0)  # Vert
 var material: StandardMaterial3D
 
 func _ready():
-    # Vérification que le RayCast est assigné
-    assert(raycast != null, "Il faut assigner un RayCast dans l'inspecteur!")
-    
-    # Vérification que le mesh est bien assigné
-    assert(mesh != null, "Il faut un MeshInstance3D dans la scène!")
-
-    # Création et configuration du matériau
-    material = StandardMaterial3D.new()
-    if material != null:
-        mesh.material_override = material
-        material.albedo_color = normal_color
-    else:
-        push_error("Le matériau n'a pas pu être créé!")
+	# Vérification que le RayCast est assigné
+	assert(raycast != null, "Il faut assigner un RayCast dans l'inspecteur!")
+	
+	# Création et configuration du matériau
+	material = StandardMaterial3D.new()
+	mesh.material_override = material
+	material.albedo_color = normal_color
 
 func _process(_delta):
-    # Vérification que raycast et material sont bien initialisés
-    if raycast != null and material != null:
-        if raycast.is_colliding() and raycast.get_collider().name != "StaticFloor":
-            material.albedo_color = detect_color  # Change en vert
-        else:
-            material.albedo_color = normal_color  # Retourne à la couleur normale
-    else:
-        push_error("Le RayCast ou le matériau n'est pas initialisé!")
+	if raycast.is_colliding() and raycast.get_collider().name != "StaticFloor":
+		material.albedo_color = detect_color  # Change en vert
+	else:
+		material.albedo_color = normal_color  # Retourne à la couleur normale
